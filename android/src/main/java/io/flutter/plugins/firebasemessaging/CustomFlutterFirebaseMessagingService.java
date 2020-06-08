ac1e1b588090bde436b147a7f04edfe34b551922
Maugost Mtellect <ammaugost@gmail.com>
2020-06-08 12:35:22 +0100

    Gradle Fix...

--- android/src/main/java/io/flutter/plugins/firebasemessaging/CustomFlutterFirebaseMessagingService.java
+++ android/src/main/java/io/flutter/plugins/firebasemessaging/CustomFlutterFirebaseMessagingService.java
@@ -2,33 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package android.src.main.java.io.flutter.plugins.firebasemessaging;
+//package android.src.main.java.io.flutter.plugins.firebasemessaging;
+package io.flutter.plugins.firebasemessaging;
 
-import android.app.ActivityManager;
-import android.app.KeyguardManager;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Handler;
-import android.os.Process;
-import android.util.Log;
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
-import com.google.firebase.messaging.FirebaseMessagingService;
+
 import com.google.firebase.messaging.RemoteMessage;
-import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.PluginRegistry;
-import io.flutter.view.FlutterCallbackInformation;
-import io.flutter.view.FlutterMain;
-import io.flutter.view.FlutterNativeView;
-import io.flutter.view.FlutterRunArguments;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.atomic.AtomicBoolean;
+import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService;
 
 public class CustomFlutterFirebaseMessagingService extends FlutterFirebaseMessagingService {
   @Override
